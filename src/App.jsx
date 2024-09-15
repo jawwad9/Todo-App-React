bf4519dc4533185ac2ab8af36d00187b2262b1fd
jawwad9 <jawwadmuhammal514@gmail.com>
2024-09-15 18:11:05 +0500

    Complete

--- src/App.jsx
+++ src/App.jsx
@@ -62,10 +62,10 @@ const App = () => {
   
   return(
     <>
-    <div className=" container d-flex flex-column align-items-center p-4 min-vh-100 bg-light">
+    <div className=" d-flex flex-column align-items-center p-4 min-vh-100 bg-light">
       <div className="">
       <h1 className="text-center mt-3">Todo App</h1>
-    <form onSubmit={addTodo} className="d-flex w-100 mb-4 mt-3"
+    <form onSubmit={addTodo} className="d-flex w-100 mb-4 mt-3 "
           style={{ maxWidth: '500px' }}>
       <input className="form-control me-2" type="text" placeholder="Enter Text" ref={todoVal} />
       <button className="btn btn-outline-primary " type="submit" >Submit</button>
@@ -74,7 +74,7 @@ const App = () => {
 
    <div className=" bg-light text-center p-5 gap-3 list-group w-100 " style={{ maxWidth: '500px' }}>
       {todo.map((item , index)=>{
-        return <div className="d-flex justify-content-around align-items-center mb-2 p-2 rounded shadow-sm" 
+        return <div className="d-flex justify-content-around align-items-center mb-2 p-2 rounded shadow-sm bg-gray-800 p-6 rounded-lg shadow-lg transition transform hover:scale-105 hover:shadow-cyan-500/50 rounded" 
         key={index}>{item}
         <div className="btn-group d-flex align-items-center justy-content-center flex-wrap">
         <button className="btn btn-outline-primary btn-sm" onClick={()=> editTodo(index)}>Edit</button>
